d6d657403453d28a46cfc057b7420cea6ecda385
Simon Leiß <git@simon-leiss.de>
2020-03-21 17:49:45 +0100

    Update first batch of icons and other settings.

--- app/src/main/java/at/xtools/pwawrapper/Constants.java
+++ app/src/main/java/at/xtools/pwawrapper/Constants.java
@@ -3,16 +3,16 @@ package at.xtools.pwawrapper;
 public class Constants {
     public Constants(){}
     // Root page
-    public static String WEBAPP_URL = "https://www.leasingrechnen.at/";
-    public static String WEBAPP_HOST = "leasingrechnen.at"; // used for checking Intent-URLs
+    public static String WEBAPP_URL = "https://symptomtracker.de";
+    public static String WEBAPP_HOST = "symptomtracker.de"; // used for checking Intent-URLs
 
-	// User Agent tweaks
+    // User Agent tweaks
     public static boolean POSTFIX_USER_AGENT = true; // set to true to append USER_AGENT_POSTFIX to user agent
     public static boolean OVERRIDE_USER_AGENT = false; // set to true to use USER_AGENT instead of default one
     public static String USER_AGENT_POSTFIX = "AndroidApp"; // useful for identifying traffic, e.g. in Google Analytics
     public static String USER_AGENT = "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Mobile Safari/537.36";
-	
-	// Constants
+
+    // Constants
     // window transition duration in ms
     public static int SLIDE_EFFECT = 2200;
     // show your app when the page is loaded XX %.
